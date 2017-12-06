454d3a2481524a714fb46c1866b8ce05d5569310
Mitchell Smoot <mitchsmoot@Mitchells-MacBook-Pro.local>
2017-12-06 16:54:02 -0700

    implemented administrate dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    resources :users
+    resources :posts
+
+    root to: "users#index"
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :posts
   devise_for :users, skip: [:registrations]
