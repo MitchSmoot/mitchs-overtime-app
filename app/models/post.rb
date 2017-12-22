100e5cb9b7621e0e7302d1dba7086a7132e852c2
Mitchell Smoot <mitchsmoot@Mitchells-MacBook-Pro.local>
2017-12-22 15:55:22 -0700

    implemented enum for status into posts for approval workflow

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 Rails.application.config.active_record.belongs_to_required_by_default = false
 
 class Post < ApplicationRecord
+  enum status: { submitted: 0, approved: 1, rejected: 2 }
   belongs_to :user
   validates_presence_of :date, :rationale
 end
