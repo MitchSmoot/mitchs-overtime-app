392e76586bdb36ed6f8340ce5a27efad53b7a184
Mitchell Smoot <mitchsmoot@Mitchells-MacBook-Pro.local>
2017-12-30 22:32:47 -0700

    implemented functionality for post status

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,3 @@
-Rails.application.config.active_record.belongs_to_required_by_default = false
-
 class Post < ApplicationRecord
   enum status: { submitted: 0, approved: 1, rejected: 2 }
   belongs_to :user
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,6 +10,10 @@
 
 puts "1 user created"
 
+AdminUser.create(email: "admin@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Admin", last_name: "User")
+
+puts "1 admin user created"
+
 100.times do |post|
   Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id)
   puts "post #{post} created!"
